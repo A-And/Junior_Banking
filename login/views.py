e1a7bf16fc950257d88e164d7f1cd4be370dc634
Teddi <teddi.orange@gmail.com>
2015-03-27 14:39:08 +0000

    - Added .gitignore for pycache files to prevent issues with collisions - Same for logs - Added 1.0 for URL

--- login/views.py
+++ login/views.py
@@ -14,15 +14,18 @@ def landing(request):
         logger = logging.getLogger(__name__)
         if form.is_valid():
             api_url = settings.API_URL
+
             post_values = {
                 'appid': settings.API_KEY,
                 'user': form.cleaned_data['email'],
                 'password': form.cleaned_data['password']
             }
             logger.debug(post_values)
+
+
             post_data = urllib.parse.urlencode(post_values).encode('ascii')
             logger.debug(post_data)
-            req = urllib.request.Request(api_url + 'cdata/request', post_data)
+            req = urllib.request.Request(api_url + cdata/request', post_data)
             logger.debug(api_url + 'cdata/request')
             response = urllib.request.urlopen(req)
             page = response.read()
@@ -32,4 +35,5 @@ def landing(request):
         form = LoginForm()
 
     return render(request, 'Landing_Page.html', {'form': form, })
+
 
