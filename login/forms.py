547832cc498b44cec498331678f3b790f36c8b30
A-And <a.andonov@ncl.ac.uk>
2015-04-11 16:28:16 +0100

    Auth updates

--- login/forms.py
+++ login/forms.py
@@ -6,7 +6,7 @@ from django import forms
 class LoginForm(forms.Form):
 
     email = forms.CharField(label='email', max_length=50, widget=forms.TextInput(attrs={'class': "input_field"}))
-    password = forms.CharField(label='password', max_length=50,   widget=forms.PasswordInput(attrs={'class': "input_field"}))
+    password = forms.CharField(label='password', max_length=50, widget=forms.PasswordInput(attrs={'class': "input_field"}))
 
 
 class TransferForm(forms.Form):
