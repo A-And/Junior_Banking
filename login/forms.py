e2134a031c6a7abe1bd1fcbceae79d034bca3249
Teddi <teddi.orange@gmail.com>
2015-04-13 12:13:24 +0100

    Tightening up design, stripped out 2x login for single login

--- login/forms.py
+++ login/forms.py
@@ -5,8 +5,8 @@ from django import forms
 
 class LoginForm(forms.Form):
 
-    email = forms.CharField(label='email', max_length=50, widget=forms.TextInput(attrs={'class': "input_field"}))
-    password = forms.CharField(label='password', max_length=50, widget=forms.PasswordInput(attrs={'class': "input_field"}))
+    email = forms.CharField(label='Username', max_length=50, widget=forms.TextInput(attrs={'class': "input_field"}))
+    password = forms.CharField(label='Password', max_length=50, widget=forms.PasswordInput(attrs={'class': "input_field"}))
 
 
 class TransferForm(forms.Form):
