7c4e2e4f2bd781ec8d25c32146c62329bffb10d6
laskowski3 <laskowski3@hotmail.com>
2015-04-15 18:52:07 +0100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
                        # Examples:
                        # url(r'^blog/', include('blog.urls')),
                        url(r'^$', 'login.views.landing', name='landing'),
+                       url(r'^home/(?P<user_id>[\w]+)/$', 'login.views.home', name='home'),
                        url(r'^account/(?P<user_id>[\w]+)/$', 'login.views.account', name='account'),
                        url(r'^goals/(?P<user_id>[\w]+)/$', 'login.views.goals', name='goals'),
                        url(r'^guide/(?P<user_id>[\w]+)/$', 'login.views.guide', name='guide'),
